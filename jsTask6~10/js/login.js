bf5258c306c15bceb5a4f8251bba23f8749daf70
crq131290 <38975801+crq131290@users.noreply.github.com>
2018-07-02 21:12:19 +0800

    111

--- jsTask6~10/js/login.js
+++ jsTask6~10/js/login.js
@@ -12,7 +12,7 @@ angular
 
             $http({
                 method: 'post',
-                url: '/carrots-admin-ajax/a/login',
+                url: 'http://dev.admin.carrots.ptteng.com/a/login',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                 // 类似原生序列化的方法,
                 // data: ('name=' +
